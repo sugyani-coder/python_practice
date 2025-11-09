32ffbb078be87341206a8d35c60a12581f84147f
Sugyani Pattnaik <sugyanip@apporchid.com>
2025-11-09 22:24:55 +0530

    welcome_python

---
+++ welcome_python.py
@@ -0,0 +1,6 @@
+# Print Welcome and then to Python on two separate lines without using two print statements.
+
+def welcome_python_function():
+    print("Welcome\n to Python")
+
+welcome_python_function()
